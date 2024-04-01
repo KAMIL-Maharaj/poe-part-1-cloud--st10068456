1f26ee3988d831e5bc31c4be03fed332c13f69b8
kamil maharaj <ST10068456@vcconnect.edu.za>
2024-04-01 16:17:41 +0200

    Add project files.

---
+++ poe part 1 cloud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace poe_part_1_cloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
